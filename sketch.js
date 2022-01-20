8a9e1712dd8c9294e23c89150dc5d055c3a892e9
ddg98 <97864289+ddg98@users.noreply.github.com>
2022-01-20 12:44:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var PaniPuri;
+
+function preload(){
+   PaniPuri=loadImage("PaniPuri.jpg")
+}
+function setup(){
+    createCanvas(700,500);
+}
+function draw(){
+    background(brownie);
+    stroke("white");
+  textSize(20);
+  fill("white");
+  text("Ingrediants:",50,50);
+  text("boiled",50,65);
+  text("2 eggs",50,90);
+  text("1 cup white sugar",50,115);
+  text("1 teaspoon vanilla extract",50,140);
+  text("⅓ cup unsweetened cocoa powder",50,165);
+  text("½ cup all-purpose flour",50,190);
+  text("¼ teaspoon salt",50,215);
+  text("¼ teaspoon baking powder ",50,240);
+  text("Method:",50,265);
+  text("step 1: Preheat oven to 350 degrees F",50,290);
+  text("(175 degrees C). Grease and flour an 8-inch square pan.",50,315);
+  text("step 2: In a large saucepan, melt 1/2 cup butter.",50,340);
+  text("Remove from heat, and stir in sugar, eggs, and 1 teaspoon vanilla.",50,365);
+  text("Beat in 1/3 cup cocoa, 1/2 cup flour, salt, and baking powder.",50,390);
+  text("Spread batter into prepared pan.",50,415);
+  text("step 3: Bake in preheated oven for 25 to 30 minutes. Do not overcook.",50,440);
+  text("ENJOY!",350,475);
+}
+
+
+
+
+
+
+
